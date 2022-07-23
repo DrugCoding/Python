8e22d0667927b2622ecc8a0e1e981009c200fda2
DrugCoding <lhs940308@gmail.com>
2022-07-23 23:59:39 +0900

    list 문법 문제 보충

--- 7m12d/Q_08.py
+++ 7m12d/Q_08.py
@@ -1,18 +1,8 @@
-from tkinter import N
+numbers = [0, 20, 100, 40, 80, 70]
 
+numbers.sort()
 
-numbers = [0, 20, 100, 40]
+num = numbers[-2:]
 
-max_namber = numbers[0]
-seceond_number = numbers[0]
-
-# 1. wjscp tntwkfmf qksqhr
-for i in numbers:
-    # 만약에, n이 최대보다 크다면
-    if max_namber < n:
-        # 최대값이 었던 것이 두번째로 큰 수
-        seceond_number = max_namber
-        max_namber = n
-    elif seceond_number < n < max_namber:
-        seceond_number = n
-print(seceond_number)
+print(2::)
+print(num[0])
--- 7m21d/swea_1989.py
+++ 7m21d/swea_1989.py
@@ -1,8 +1,7 @@
 a = int(input())
-
 for i in range(1, a +1):
     b = input()
     if b == b[::1]:
-        print(1)
+        print(f'#{i} 1')
     else:
-        print(0)
+        print(f'#{i} 0')
