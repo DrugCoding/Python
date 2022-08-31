67ac71520aade271d50c8bcb4d080f3cb2c8af2d
DrugCoding <lhs940308@gmail.com>
2022-08-31 22:55:06 +0900

    8월31일수요일 코테스터디,SWEA문제풀이

---
+++ 8월31일 수요일/7785 회사에 있는 사람.py
@@ -0,0 +1,35 @@
+# 1번 풀이 (백준 실패)
+n = int(input())
+dic = {}
+for _ in range(n):
+    a, b = input().split()
+    if a in dic:
+        dic[a] = 'leave'
+    else:
+        dic[a] = 'enter'
+
+lis = []
+for k, v in dic.items():
+    if v == 'enter':
+        lis.append(k)
+lis.sort(reverse=True)
+
+print(*lis, sep = '\n')
+
+# 2번 풀이 (백준 통과)
+n = int(input())
+dic = {}
+for _ in range(n):
+    a, b = input().split()
+    dic[a] = b
+
+lis = [k for k, v in dic.items() if v == 'enter']
+lis = []
+# for k, v in dic.items():
+#     if v == 'enter':
+#         lis.append(k)
+
+lis.sort()
+lis.reverse()
+print(*lis, sep = '\n')
+
---
+++ 8월31일 수요일/s2019 더블더블.py
@@ -0,0 +1,4 @@
+a = int(input())
+lis = []
+for i in range(1, a+1):
+    print((2**i), end=' ')
