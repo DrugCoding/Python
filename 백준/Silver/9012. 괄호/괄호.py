c64b7f731f797bff539fbc9ed260c612c5412299
HS <92349357+DrugCoding@users.noreply.github.com>
2022-10-08 15:56:30 +0900

    [Silver IV] Title: 괄호, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,16 @@
+t = int(input())
+for i in range(t):
+    s = list(input())
+    sum = 0
+    for i in s:
+        if i == '(':
+            sum += 1
+        elif i == ')':
+            sum -= 1
+        if sum < 0:
+            print('NO')
+            break
+    if sum > 0:
+        print('NO')
+    elif sum == 0:
+        print('YES')
