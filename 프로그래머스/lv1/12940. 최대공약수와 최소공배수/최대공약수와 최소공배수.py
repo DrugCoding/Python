2fb3a3866326b7e2845db99b149ed185a4b89f67
HS <92349357+DrugCoding@users.noreply.github.com>
2022-11-18 01:02:50 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,10 @@
+def gcd(n, m):
+    mod = m % n
+    if mod != 0:
+        m, n = n, mod
+        return gcd(n, m)
+    else:
+        return n
+
+def solution(n,m):
+    return [gcd(n, m), int(m * n / gcd(n, m))]
