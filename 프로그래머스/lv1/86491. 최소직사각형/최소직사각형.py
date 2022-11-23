7c8071ccc13041f00e4f5b107257ab206177b937
HS <92349357+DrugCoding@users.noreply.github.com>
2022-11-23 23:38:55 +0900

    [level 1] Title: 최소직사각형, Time: 2.60 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,12 @@
+def solution(sizes):
+    w = []
+    h = []
+    for i in range(len(sizes)):
+        if sizes[i][0] >= sizes[i][1]:
+            w.append(sizes[i][0])
+            h.append(sizes[i][1])
+        else:
+            h.append(sizes[i][0])
+            w.append(sizes[i][1])
+
+    return max(w) * max(h)
