a1100fca8136b1d93124898466ff24fece76fdae
HS <92349357+DrugCoding@users.noreply.github.com>
2022-12-27 22:26:01 +0900

    [Silver V] Title: 소수, Time: 544 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2581. 소수/소수.py
@@ -0,0 +1,19 @@
+start_num = int(input())
+last_num = int(input())
+
+sosu_list = []
+for num in range(start_num, last_num+1):
+    error = 0
+    if num > 1 :
+        for i in range(2, num):
+            if num % i == 0:
+                error += 1
+                break
+        if error == 0:
+            sosu_list.append(num)
+            
+if len(sosu_list) > 0 :
+    print(sum(sosu_list))
+    print(min(sosu_list))
+else:
+    print(-1)
