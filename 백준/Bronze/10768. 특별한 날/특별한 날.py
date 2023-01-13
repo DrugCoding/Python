a516f1b8d4d0d1a31d6d38b1bcbc19a9504402d2
HS <92349357+DrugCoding@users.noreply.github.com>
2023-01-14 00:03:28 +0900

    [Bronze IV] Title: 특별한 날, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+if a > 2 :
+    print('After')
+
+elif a < 2:
+    print('Before')
+
+elif a == 2 and b == 18:
+    print('Special')
+elif a == 2 and b > 18:
+    print('After')
+elif a == 2 and b < 18:
+    print('Before')
