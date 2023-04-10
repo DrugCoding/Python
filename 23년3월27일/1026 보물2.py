36d38afa1f22572ee575f2676807a43a90e085ec
DrugCoding <lhs940308@gmail.com>
2023-04-10 22:12:30 +0900

    백준문제풀이

---
+++ 23년3월27일/1026 보물2.py
@@ -0,0 +1,17 @@
+
+ * branch            master     -> FETCH_HEAD
+Already up to date.
+
+Administrator@DESKTOP-22HO85P MINGW64 /d/지우면안됨/Desktop/Git/Python (master)
+$ git add.
+git: 'add.' is not a git command. See 'git --help'.
+
+The most similar command is
+        add
+
+Administrator@DESKTOP-22HO85P MINGW64 /d/지우면안됨/Desktop/Git/Python (master)
+$ git add .
+
+Administrator@DESKTOP-22HO85P MINGW64 /d/지우면안됨/Desktop/Git/Python (master)
+$ git commit -m '백준 문제풀이'
+On branch master
