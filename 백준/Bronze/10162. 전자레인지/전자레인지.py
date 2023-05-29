83acb9d39204dbe755d920928621ebf3d4504dd0
HS <92349357+DrugCoding@users.noreply.github.com>
2023-05-29 22:15:01 +0900

    [Bronze III] Title: 전자레인지, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.py
@@ -0,0 +1,11 @@
+time = int(input())
+
+if time % 10 != 0:
+    print("-1")
+else:
+    a = b = c = 0
+    a = time // 300
+    b = (time % 300) // 60
+    c = (time % 300) % 60 // 10
+    print(a, b, c)
+    
