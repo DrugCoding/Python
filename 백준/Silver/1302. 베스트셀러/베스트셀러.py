cd337726c1ba0dcb47c6b1b55a5a121da31a912f
HS <92349357+DrugCoding@users.noreply.github.com>
2023-06-24 10:22:21 +0900

    [Silver IV] Title: 베스트셀러, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -0,0 +1,23 @@
+a = int(input())
+dic = {}
+for i in range(a):
+    book = input()
+    if book in dic:
+        dic[book] += 1
+    else:
+        dic[book] = 1
+# print(dic)
+
+value_ = []
+for j in dic:
+    value_.append(dic[j])
+
+max_ = max(value_)
+
+key_ = []
+for k, v in dic.items():
+    if v == max_:
+        key_.append(k)
+
+key_.sort()
+print(key_[0])
